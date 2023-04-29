8e7572d917f5cb1179b28b5992f8171dcc90b563
Ray Christian <rchristian531961@yahoo.com>
2023-04-29 16:54:12 -0500

    Completed All except extension for #10

--- Fundamentals-A.js
+++ Fundamentals-A.js
@@ -291,7 +291,7 @@ function randomDelay(){
     })
 }
 
-randomDelay().then(()=>{console.log("then")}).catch(()=>{console.log("Catch: ")});
+// randomDelay().then(()=>{console.log("then")}).catch(()=>{console.log("Catch: ")});
 
 
 
@@ -299,23 +299,35 @@ randomDelay().then(()=>{console.log("then")}).catch(()=>{console.log("Catch: ")}
 /*10*/
 //run 'npm init' and accept all the defaults
 //run 'npm install node-fetch'
-//add this line to package.json after line 5: "type": "module",
+//add this line to package.json after line 5: "type": "module"
 
-/*import fetch from 'node-fetch'
+import fetch from 'node-fetch'
 globalThis.fetch = fetch
 function fetchURLData(url) {
-let fetchPromise = fetch(url).then(response => {
-if (response.status === 200) {
-return response.json();
-} else {
-throw new Error(`Request failed with status ${response.status}`);
+    let fetchPromise = fetch(url).then(response => {
+        if (response.status === 200) {
+            return response.json();
+        } 
+        else {
+            throw new Error(`Request failed with status ${response.status}`);
+        }
+    });
+    return fetchPromise;
 }
-});
-return fetchPromise;
-}
+
 fetchURLData('https://jsonplaceholder.typicode.com/todos/1')
 .then(data => console.log(data))
-.catch(error => console.error(error.message));*/
+.catch(error => console.error(error.message));
 
+async function fetchUdata(url){
+    const fetchPromise= await fetch(url);
+    //Everything Below this line is waiting on the fetch request from above due to await
+    if (!fetchPromise.status === 200) {
+        throw new Error(`Request failed with status ${fetchPromise.status}`);  
+    } 
+    const data=await fetchPromise.json();
+        return data;
+}
 
-
+fetchUdata('https://jsonplaceholder.typicode.com/todos/1').then(data => console.log(data))
+.catch(error => console.error(error.message));
